7492d07ac8b9827075af2f596dc8387c156a059a
Vlad <vyuzkevych93@gmail.com>
2024-07-15 17:11:13 +0300

    home work 3

---
+++ js-variables/variables.js
@@ -0,0 +1,22 @@
+
+// task 1
+let adminName = "Jonh";
+let userName = "Jack";
+
+console.log(typeof adminName, typeof userName);
+
+// task 2
+let awesome = "awesome";
+let variable1 = "really";
+let variable2 = "hell yeah!";
+
+console.log(`Javascript is: ${variable1 + " " 
+    + awesome + ", " 
+    + variable2}`
+);
+
+// task 3
+let testVariable = 10;
+testVariable = 20;
+
+console.log(testVariable);
